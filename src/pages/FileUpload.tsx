ead09650722b8b68fba791b8540a1b0366173609
ej070961 <ej070961@gmail.com>
2024-08-27 16:14:35 +0900

    feat: 서비스 선택 기능 구현(#10)

--- src/pages/FileUpload.tsx
+++ src/pages/FileUpload.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import CreateLayout from "../components/CreateLayout";
 import Progress from "../components/Create/Progress";
 import * as C from "../styles/create.style";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 function FileUpload() {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const { menu } = location.state;
+  console.log("state", location.state);
 
   const goToNextPage = () => {
-    navigate("/create/service-selection");
+    navigate("/create/analysis-result");
   };
   return (
     <CreateLayout>
-      <Progress currentStep={0} />
+      <Progress currentStep={1} />
       <div className="flex flex-col items-end">
         <C.ContentWrapper></C.ContentWrapper>
         <C.NextButton onClick={goToNextPage}>Next →</C.NextButton>
